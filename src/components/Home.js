1706fb3e0115e6f059afda57e7d152a09ac211d5
Pablo Castro <pablocas-tro1@outlook.com>
2023-07-28 22:33:58 -0400

    Complete Lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const H1 = () => {
+  return (
+    <h1 style={{ color: "firebrick" }}>
+      {name} is a Web Developer from {city}
+    </h1>
+  );
+};
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <H1 />
+    </div>
+  );
 }
 
 export default Home;
